f6a490f94f31e1a0e4520054617cf879d1ab65ae
unknown <you@example.com>
2024-10-28 02:46:40 +0800

    notes page

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,53 +1,59 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
-    <title>Note App</title>
-</head>
-<body>
-    <h1>Notes</h1>
-    <form action="{{ route('createNote') }}" method="GET">
-        <button type="submit">New Note</button>
-    </form>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <title>Notes</title>
+  </head>
 
-    <form action="{{ route('notes') }}" method="GET">
-        <div>
-            <input type="search" name="search" id="search" class="form control" placeholder="Search" value="{{ $search }}">
+  <body>
+    <div class="flex items-center justify-center bg-purple-800 p-2">
+      <h1 class="prose text-white text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl"><a href="/notes">MyNotes</a></h1>
+    </div>
+
+    <div class="flex w-full items-center justify-center">
+      <form action="{{ route('notes') }}" method="GET" class="flex w-full max-w-screen-lg items-center space-x-2 px-4">
+        <div class="relative flex-grow p-1">
+          <input class="bg-white-500 w-full py-2 pl-10 pr-3 shadow-md focus:outline-purple-900" type="search" name="search" id="search" placeholder="&#x1F50E;&#xFE0E;" value="{{ $search }}" />
         </div>
-        <button>Search</button>
-        <a href="{{ url('/notes') }}">Reset</a>
-    </form>
+        <button class="whitespace-nowrap rounded-md bg-purple-500 px-3 py-2 text-white active:bg-purple-300">Search</button>
+        <a href="{{ url('/notes') }}" class="whitespace-nowrap text-violet-900 active:text-violet-500">Reset</a>
+      </form>
+    </div>
 
-    <hr>
+      <div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
 
-    @foreach ($notes as $note)
-        <div>
-            <b>{{ $note->title }}</b><br>
-            <i>{{ $note->description }}</i><br>
-            {{ $note->content }}<br>
-            Updated on {{ $note->updated_at }}<br>
-            Created on {{ $note->created_at }}
-            <form action="{{ route('viewNote', ['id' => $note->id])}}" method="GET">
-                <button type="submit">View Note</button>
+        <form action="{{ route('createNote') }}" method="GET">
+            <button type="submit" class="items-center w-full rounded-lg bg-gradient-to-tr from-purple-900 to-purple-500 text-white  shadow-md flex flex-col justify-between"><img class="w-52 h-52" src="{{ asset('assets/plus.png') }}"></button>
+        </form>
+
+        @foreach ($notes as $note)
+        <div class="h-52 p-4 w-full rounded-lg bg-purple-500 text-white  shadow-md flex flex-col justify-between text-sm sm:text-md md:text-lg xl:text-xl">
+          <div>
+            <h2 class="text-lg font-bold text-purple-950">{{ $note->title }}</h2>
+            <b class="line-clamp-4 font-normal text-white">{{ $note->content }}</b>
+          </div>
+
+          <div class="mt-2 flex justify-end space-x-2">
+            <form action="{{ route('viewNote', ['id' => $note->id]) }}" method="GET">
+              <button type="submit" class="text-white"><span class="material-icons">visibility</span></button>
             </form>
 
-            <form action="{{ route('addToFavorites', ['id' => $note->id]) }}" method="POST">
-                @method('PATCH')
-                @csrf
-                <button type="submit">Add To Favorites</button>
+            <form action="{{ route('editNote', ['id' => $note->id]) }}" method="GET">
+              <button type="submit" class="text-white"><span class="material-icons">edit</span></button>
             </form>
 
-            <form action="{{ route('deleteNote', ['id' => $note->id]) }}" method="POST"
-                onsubmit="return confirm('Are you sure you want to delete this note?')">
-                @method('DELETE')
-                @csrf 
-                <button type="submit">Delete Note</button>
+            <form action="{{ route('deleteNote', ['id' => $note->id]) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this note?')">
+              @method('DELETE') @csrf
+              <button type="submit" class="text-white"><span class="material-icons">delete</span></button>
             </form>
-
-            <hr>
+          </div>
         </div>
-    @endforeach
-</body>
+        @endforeach
+      </div>
+    </div>
+  </body>
 </html>
